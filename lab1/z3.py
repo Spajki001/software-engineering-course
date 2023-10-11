b435be5546162e10463a657fe482825e99b55ff6
mateo.spajic <spajic.mateo@gmail.com>
2023-10-11 18:44:21 +0200

    Modified z3.py

--- lab1/z3.py
+++ lab1/z3.py
@@ -4,11 +4,12 @@
 # of the list that are less than 5.
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+number = int(input("Unesi broj: "))
 
 newlist = []
 
 for el in a:
-    if el < 5:
+    if el < number:
         newlist.append(el)
 
 print(newlist)
