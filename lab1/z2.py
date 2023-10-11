9b8ceb2a82a863fcd7e9c23c5d313176b0801b80
mateo.spajic <spajic.mateo@gmail.com>
2023-10-11 18:31:12 +0200

    Finished z2.py

--- lab1/z2.py
+++ lab1/z2.py
@@ -3,6 +3,13 @@
 # appropriate message to the user.
 
 number = int(input("Unesi broj: "))
+num = int(input("Unesi djeljenik: "))
+check = int(input("Unesi djelitelj: "))
+
+if num % check == 0:
+    print(f"\nBroj {num} je djeljiv sa {check}\n")
+else:
+    print(f"Broj {num} nije dijeljiv sa {check}")
 
 if (number % 4 == 0):
     print("Uneseni broj je dijeljiv sa 4")
