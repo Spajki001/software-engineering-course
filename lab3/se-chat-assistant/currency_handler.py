4d4de7a8785db336d2b30faf042d6eb955ce0c61
mateo.spajic <spajic.mateo@gmail.com>
2023-10-25 20:48:13 +0200

    Fixed code

--- lab3/se-chat-assistant/currency_handler.py
+++ lab3/se-chat-assistant/currency_handler.py
@@ -4,22 +4,24 @@ import json
 
 class CurrencyHandler(QueryHandlerBase):
     def can_process_query(self, query):
-        if "currency" in query:
+        if "exchange" in query:
             return True
         return False
     
     def process(self, query):
         try:
-            value = query.split()[1]
-            result = self.call_api(value)*100
-            self.ui.say(f"{value} EUR is {result} GBP")
+            currency = query.split()
+            currency1 = currency[1]
+            currency2 = currency[2]
+            result = self.call_api(currency1, currency2)
+            self.ui.say(f"The exchange rate between {currency1} and {currency2} is {result}.")
         except:
             self.ui.say("Oh no! There was an error trying to contact Currency api.")
             
-    def call_api(self, value):
+    def call_api(self, currency1, currency2):
         url = "https://currency-exchange.p.rapidapi.com/exchange"
         
-        querystring = {"from":"EUR","to":"GBP","q":f"{value}"}
+        querystring = {"from":f"{currency1}","to":f"{currency2}","q":"1"}
         
         headers = {
 			"X-RapidAPI-Key": "041d4bffadmsh54d6ef9d2437b3ep18bd10jsnc77e8bae94c7",
