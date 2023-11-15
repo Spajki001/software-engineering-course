c4fe39f4a244d17f9048fc42ac011eae2e6326c1
mateo.spajic <spajic.mateo@gmail.com>
2023-11-15 22:06:38 +0100

    Added missing toolbar

--- lab5/mysite/mysite/settings.py
+++ lab5/mysite/mysite/settings.py
@@ -15,10 +15,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 # Application definition
 
 INSTALLED_APPS = [
+    "debug_toolbar",
     "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -36,6 +39,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "mysite.urls"
