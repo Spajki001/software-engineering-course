e15e7133291fd7c6f8fa6f50efc7f20258fe3474
mateo.spajic <spajic.mateo@gmail.com>
2023-11-29 17:24:27 +0100

    Made changes to lab7

--- lab7/myimgur/images/views.py
+++ lab7/myimgur/images/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Image
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.utils import timezone
 
 # Create your views here.
 
@@ -24,3 +25,15 @@ def image_detail(request, image_id):
         'image': image,
     }
     return render(request, 'images/image_detail.html', context)
+
+def create_image(request: HttpRequest):
+    if request.method == 'POST':
+        title = request.POST.get('title', "")
+        url = request.POST.get('url', "")
+        pub_date = request.POST.get('pub_date', timezone.now())
+        desc = request.POST.get('desc', "")
+        image = Image(title=title, url=url, pub_date=pub_date, desc=desc)
+        image.save()
+        return HttpResponseRedirect(f'/images/{image.id}')
+    context = {}
+    return render(request, 'images/create_image.html', context)
