d0e013bfd8613a6d212b4f95a570ef89fce934b7
mateo.spajic <spajic.mateo@gmail.com>
2023-12-06 18:02:28 +0100

    Made migrations and added new def in comments models.py

--- lab8/myimgur/images/models.py
+++ lab8/myimgur/images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -13,10 +14,13 @@ class Image(models.Model):
         return self.title
     
 class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
-    author = models.CharField(max_length=128)
     content = models.CharField(max_length=512)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.author}: {self.content[:30]}"
+        return f"{self.user.username}: {self.content[:30]}"
+    
+    def author(self):
+        return f"{self.user.username}"
